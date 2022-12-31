61a9621e2a4e8ad03585923f2e415cbe55c163b2
mamimo2353 <115897973+mamimo2353@users.noreply.github.com>
2022-12-31 17:31:19 +0300

    Add files via upload

---
+++ todolist/Models/Atp11Context.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace todolist.Models;
+
+public partial class Atp11Context : DbContext
+{
+    public Atp11Context()
+    {
+    }
+
+    public Atp11Context(DbContextOptions<Atp11Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Liste> Listes { get; set; }
+
+    public virtual DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;port=3306;user=root;database=atp11", Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.40-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("latin1_swedish_ci")
+            .HasCharSet("latin1");
+
+        modelBuilder.Entity<Liste>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("liste");
+
+            entity.Property(e => e.Ad).HasMaxLength(50);
+            entity.Property(e => e.Soyad).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Todo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("todo");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+            entity.Property(e => e.Iscomplated)
+                .HasDefaultValueSql("b'0'")
+                .HasColumnType("bit(1)");
+            entity.Property(e => e.Title).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ todolist/Models/Liste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace todolist.Models;
+
+public partial class Liste
+{
+    public string Ad { get; set; } = null!;
+
+    public string Soyad { get; set; } = null!;
+
+    public DateOnly Tarih { get; set; }
+}
